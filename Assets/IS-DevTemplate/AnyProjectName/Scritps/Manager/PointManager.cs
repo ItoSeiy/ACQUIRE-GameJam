cbbe4b326137503ba9cad5b474c7e5735d35e357
伊東聖矢 <itoseiya1219@gmail.com>
2022-08-18 13:50:01 +0900

    pointmanager wip

---
+++ Assets/IS-DevTemplate/AnyProjectName/Scritps/Manager/PointManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void SetEvent()
+    {
+        // TODO: プレイヤーのポイント獲得時のイベントを購読
+    }
+
+
+}
