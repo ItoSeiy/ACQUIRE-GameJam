8739615d1b9665009dd8ff000a0061d69498c6ed
TaitoAnzai <taito034166@gmail.com>
2022-08-19 13:21:39 +0900

    プレイヤーのスタン関数実装

--- Assets/IS-DevTemplate/AnyProjectName/Scritps/Player/Player.cs
+++ Assets/IS-DevTemplate/AnyProjectName/Scritps/Player/Player.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 public class Player : SingletonMonoBehaviour<Player>
 {
+    private bool CanMove = true;
+
     [SerializeField]
     public float _speedx = 0.1f;
     public float _speedy = 0.05f;
@@ -14,11 +16,15 @@ public class Player : SingletonMonoBehaviour<Player>
 
     private void Start()
     {
-        this.GetComponent<SpriteRenderer>().flipX = true;
+        //this.GetComponent<SpriteRenderer>().flipX = true;
     }
 
     private void Update()
     {
+        if (!CanMove) return;
+
+        Vector3 reversal = Vector3
+
         // TODO: キー入力で移動　進行方向で画像反転
         if (Input.GetKey(KeyCode.W))
         {
@@ -31,24 +37,33 @@ public class Player : SingletonMonoBehaviour<Player>
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(_speedx, 0f, 0f);
-            this.GetComponent<SpriteRenderer>().flipX = true;
+            //this.GetComponent<SpriteRenderer>().flipX = true;
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-_speedx, 0f, 0f);
-            this.GetComponent<SpriteRenderer>().flipX = false;
-        }
+            //this.GetComponent<SpriteRenderer>().flipX = false;
+            transform.localScale.x = -1f;
+        } 
 
     }
 
     public async void Stan(int stanTime)
     {
+        CanMove = false;
+
         await Task.Delay(stanTime * 1000);
+        CanMove = true;
     }
     private void OnTriggerEnter2D(Collider2D other)    
     {
         // TODO: プレイヤーを引き寄せる実装をする　引き寄せ終わったらデストロイ
-        Destroy(other.gameObject);
+        People people = other.GetComponent<People>(); 
+        if (people)
+        { 
+            people.
+        }
+       
     }
     
 }
